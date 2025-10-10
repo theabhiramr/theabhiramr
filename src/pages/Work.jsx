4c2a7a0d4502f8ba87aa97bd608626d8576aabaf
theabhiramr <ramachandran.abhiram@gmail.com>
2025-10-10 16:26:49 -0400

    added placeholders

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,3 +1,10 @@
-const Work = () => {}
+const Work = () => {
+    return (
+        <div className="p-4">
+            <h1 className="font-outfit text-2xl font-bold mb-4">🚧 Coming Soon!</h1>
+            <p className="font-geist-mono text-content">This page is under construction. Please check back later!</p>
+        </div>
+    );
+}
 
 export default Work;
