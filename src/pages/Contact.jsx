4c568ef27f3d0ad13e8a462ea3a680ae3dbb5931
theabhiramr <ramachandran.abhiram@gmail.com>
2025-10-14 03:06:53 -0400

    (literal) window dressing

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,12 @@
+import React, { useEffect } from "react";
+
 const Contact = () => {
+    useEffect(() => {
+            document.title = "Abhi Ramachandran | Contact";
+            window.scrollTo(0, 0);
+          }, []);
     return (
-        <div className="p-4">
+        <div className="px-6 lg:px-32 py-6">
             <h1 className="font-outfit text-2xl font-bold mb-4">🚧 Coming Soon!</h1>
             <p className="font-geist-mono text-content">This page is under construction. Please check back later!</p>
         </div>
